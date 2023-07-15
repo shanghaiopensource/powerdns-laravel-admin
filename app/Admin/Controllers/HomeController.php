057c06026e71049796eb79ea9fc7fe2e1bb3a90e
root <root@localhost.localdomain>
2023-07-15 15:42:05 +0800

    add sql for pdns backend

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,22 +13,19 @@ class HomeController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->title('Dashboard')
+            ->title('PowerDNS Admin')
             ->description('Description...')
             ->row(Dashboard::title())
             ->row(function (Row $row) {
 
+                /*
                 $row->column(4, function (Column $column) {
                     $column->append(Dashboard::environment());
                 });
 
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
                     $column->append(Dashboard::dependencies());
-                });
+                });*/
             });
     }
 }
