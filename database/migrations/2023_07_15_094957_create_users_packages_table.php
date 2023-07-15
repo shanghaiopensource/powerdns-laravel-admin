445fe1d33c7118c826dc7cf0d275c17391de502b
Steve <Steven@zylinkus.com>
2023-07-15 23:03:20 +0800

    fix

---
+++ database/migrations/2023_07_15_094957_create_users_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_packages', function (Blueprint $table) {
+            $table->id();
+            $table->integer("user_id")->default(0)->comment("package user");
+            $table->integer("package_id")->default(0)->comment("package id");
+            $table->date("start_at")->comment("package start date");
+            $table->date("end_at")->comment("package end date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_packages');
+    }
+}
