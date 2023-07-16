3f168267c848c2b79872f4f1ef649449cf86b200
Steve <Steven@zylinkus.com>
2023-07-16 22:16:27 +0800

    fix laravel admin to 1.0.4

--- app/Http/Controllers/Api/V1/ServerController.php
+++ app/Http/Controllers/Api/V1/ServerController.php
@@ -23,21 +23,11 @@ class ServerController extends BaseController
 
         // Create a new zone.
         $zone = $powerdns->createZone(
-            'example.com',
-            ['ns1.example.com.', 'ns2.example.com.']
+            'zylinkus.com',
+            ['ns1.zylinkus.com.', 'ns2.zylinkus.com.']
         );
 
-        var_dump($powerdns,$zone);exit;
 
-        $client = new \GuzzleHttp\Client(['debug'=>true]);
-
-        $res = $client->request('POST', $url."/api/v1/servers", ['header'=>[
-            'X-API-Key' => $key,
-            'Accept' => 'application/json',
-            'Content-Type' => 'application/json',
-
-        ]]);
-
-        var_dump($res);
+        
     }
 }
